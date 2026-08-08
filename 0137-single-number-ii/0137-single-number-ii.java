f0e3b12f8fa145e8fddd6e7b2c58fc26976c24bb
Umar Khan <uk08.ff@gmail.com>
2026-08-09 00:50:05 +0530

    Time: 8 ms (13.79%), Space: 45.5 MB (40.91%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,10 @@
+ class Solution {
+    public int singleNumber(int[] nums) {
+        //your code goes here
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i=i+3)
+        {
+            if(nums[i]!=nums[i-1]) return nums[i-1];
+        } return nums[nums.length-1];
+    }
+}
