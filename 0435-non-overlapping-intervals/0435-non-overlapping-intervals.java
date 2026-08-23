edca6c20dab1286441b2692d629a9027eec045f9
Umar Khan <uk08.ff@gmail.com>
2026-08-24 01:30:51 +0530

    Time: 47 ms (60.17%), Space: 115.7 MB (58.07%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int comp(int[] val1, int[] val2) {
+        return Integer.compare(val1[1], val2[1]);
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, Solution::comp);
+
+        int cnt = 1;
+        int lastEndTime = intervals[0][1];
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= lastEndTime) {
+                cnt++;
+                lastEndTime = intervals[i][1];
+            }
+        }
+
+        return n - cnt;
+    }
+}
