c18669e4303da3e01bcf5fb9576f030974fec078
Umar Khan <uk08.ff@gmail.com>
2026-08-30 23:42:28 +0530

    Time: 2 ms (99.63%), Space: 52.4 MB (12.49%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int candy(int[] ratings) {
+          int n=ratings.length;
+          int i=1,sum=1;
+          while(i<n)
+          {
+            if(ratings[i]==ratings[i-1])
+            { 
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1])
+            {
+               
+               peak++; sum+=peak;
+               i++;
+              
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1])
+            {
+                sum+=down;
+                down++;
+                i++;
+            } if(peak< down)
+            {
+                sum+= (down-peak) ;
+            }
+          } return sum;
+    }
+}
