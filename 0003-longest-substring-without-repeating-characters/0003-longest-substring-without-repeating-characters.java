485b8bfe76455d320d4292deb7bebfe7729e9a09
Umar Khan <uk08.ff@gmail.com>
2026-09-05 01:30:57 +0530

    Time: 9 ms (61.57%), Space: 47.7 MB (53.99%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        int hash[]=new int[256];
+        Arrays.fill(hash,-1);
+        int l=0,r=0,maxlen=0;
+        while(r<n)
+        {
+            if(hash[s.charAt(r)]!=-1)
+            {
+              l=Math.max(hash[s.charAt(r)] + 1, l);  
+            } int len=r-l+1;
+            maxlen=Math.max(len,maxlen);
+            hash[s.charAt(r)]=r;
+            r++;
+        } return maxlen;
+    }
+}
