ae99e8f526208879ffc473c54746e0b8d6a23687
Umar Khan <uk08.ff@gmail.com>
2026-09-06 23:48:11 +0530

    Time: 53 ms (80.22%), Space: 68.5 MB (87.15%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,33 +1,41 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int l = 0, r = 0, maxlen = 0;
-
-        HashMap<Integer, Integer> mp = new HashMap<>();
-
-        while (r < fruits.length) {
-
-            if (mp.containsKey(fruits[r])) {
-                mp.put(fruits[r], mp.get(fruits[r]) + 1);
-            } else {
-                mp.put(fruits[r], 1);
-            }
-
-            while (mp.size() > 2) {
-
-                mp.put(fruits[l], mp.get(fruits[l]) - 1);
-
-                if (mp.get(fruits[l]) == 0) {
-                    mp.remove(fruits[l]);
+        int n = fruits.length;
+        
+        /* Variable to store the 
+        maximum length of substring */
+        int maxLen = 0;  
+        
+        /* Map to track the count of each
+        fruit type in the current window */
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        
+        // Pointers for the sliding window approach
+        int l = 0, r = 0;
+        
+        while (r < n) {
+            mpp.put(fruits[r], mpp.getOrDefault(fruits[r], 0) + 1);
+            
+            /* If number of different fruits exceeds
+             2 shrink the window from the left */
+            if (mpp.size() > 2) {
+                mpp.put(fruits[l], mpp.get(fruits[l]) - 1);
+                if (mpp.get(fruits[l]) == 0) {
+                    mpp.remove(fruits[l]);
                 }
-
                 l++;
             }
-
-            maxlen = Math.max(maxlen, r - l + 1);
-
+            
+            /* If number of different fruits 
+            is at most 2, update maxLen */
+            if (mpp.size() <= 2) {
+                maxLen = Math.max(maxLen, r - l + 1);
+            }
+            
             r++;
         }
-
-        return maxlen;
+        
+        // Return the maximum fruit
+        return maxLen;
     }
 }
